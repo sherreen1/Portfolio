168837c50913e5931ec47628d45a1e3d4b9cde3a
Sherien Ayed <Shero@email.com>
2026-09-16 17:35:27 +0300

    Initial commit

---
+++ js/theme.js
@@ -0,0 +1,41 @@
+const themeToggle = document.getElementById("themeToggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    updateThemeIcon(true);
+}
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    const isDark =
+        document.body.classList.contains("dark-mode");
+
+    localStorage.setItem(
+        "theme",
+        isDark ? "dark" : "light"
+    );
+
+    updateThemeIcon(isDark);
+});
+
+
+function updateThemeIcon(isDark) {
+
+    const icon = themeToggle.querySelector("i");
+
+    if (isDark) {
+
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+
+    } else {
+
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+
+    }
+}
